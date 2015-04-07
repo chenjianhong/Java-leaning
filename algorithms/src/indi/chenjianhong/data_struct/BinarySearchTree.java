52ed732c83231e3aa0d5f479a0a638ab0e024c92
chenjianhong <1815515831@qq.com>
2015-04-08 01:12:57 +0800

    complete the binary tree

--- algorithms/src/indi/chenjianhong/data_struct/BinarySearchTree.java
+++ algorithms/src/indi/chenjianhong/data_struct/BinarySearchTree.java
@@ -4,7 +4,7 @@ package indi.chenjianhong.data_struct;
 /**
  * own search tree
  */
-public class BinarySearchTree<AnyType> implements Comparable<AnyType>{
+public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>{
 
     private BinaryNode<AnyType> root;
 
@@ -26,19 +26,47 @@ public class BinarySearchTree<AnyType> implements Comparable<AnyType>{
     }
 
 
-    public boolean insert(AnyType element_data){
-        insert(element_data,root);
-        return true;
+    public void insert(AnyType element_data){
+        root = insert(element_data,root);
     }
 
-    public boolean insert(AnyType element_data,BinaryNode<AnyType> parent_node){
+    public BinaryNode<AnyType> insert(AnyType element_data,BinaryNode<AnyType> parent_node){
         if(parent_node == null){
-            parent_node = new BinaryNode<AnyType>(null,null,element_data);
+            return new BinaryNode<AnyType>(null,null,element_data);
         }
-        return true;
+        int compare_result = element_data.compareTo(parent_node.data);
+        if(compare_result>0){
+            parent_node.right = insert(element_data, parent_node.right);
+        }
+        else if(compare_result<0){
+            parent_node.left = insert(element_data,parent_node.left);
+        }
+        return parent_node;
     }
 
-    public
+    public void print_tree(){
+        print_tree(root);
+    }
+
+    public void print_tree(BinaryNode<AnyType> parent_node){
+        System.out.println(String.format("%s",parent_node.data));
+        if (parent_node.right!= null){
+            print_tree(parent_node.right);
+        }
+        if (parent_node.left!= null){
+            print_tree(parent_node.left);
+        }
+    }
+
+    public static void main(String args[]){
+        BinarySearchTree<Integer> b_tree = new BinarySearchTree<Integer>();
+        b_tree.insert(1);
+        b_tree.insert(2);
+        b_tree.insert(3);
+        b_tree.insert(4);
+        b_tree.print_tree();
+    }
+
 
 
 }
